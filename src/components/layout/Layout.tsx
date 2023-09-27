9a5308e7fcf56a57c1fdce56af69dae88be469a3
Alikley <88429641+Alikley@users.noreply.github.com>
2023-09-27 21:46:50 +0330

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,10 +2,24 @@ import { Outlet } from 'react-router-dom'
 import Footer from './footer/Footer'
 import Header from './header/Header'
 
-function Layout() {
+export interface CardItemType{
+  id:number;
+  category:string
+  description:string;
+  image:string;
+  price:number;
+  title:string
+  amount:number
+}
+
+interface StateType{
+  cardItems:CardItemType[]
+  setCardItems:React.Dispatch<React.SetStateAction<CardItemType[]>>
+ }
+function Layout({cardItems,setCardItems}:StateType) {
   return (
     <div>
-        <Header />
+        <Header cardItems={cardItems} setCardItems={setCardItems} />
         <Outlet />
         <Footer />
     </div>
