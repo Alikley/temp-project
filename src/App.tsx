f76f8886e15764e9df14ea7d608a2a2d29da853a
Alikley <88429641+Alikley@users.noreply.github.com>
2023-09-27 21:46:00 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
 import { Routes, Route, Navigate,  } from "react-router-dom";
 
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { LoginContext } from './context/LoginContext';
 import Home from './components/home/Home';
 import Layout from './components/layout/Layout';
 import Login from './components/login/Login';
 import Panel from './components/panel/Panel';
 
-
+export interface CardItemType{
+  id:number;
+  category:string
+  description:string;
+  image:string;
+  price:number;
+  title:string
+  amount:number
+ }
 
 function App() {
   const userLog = useContext(LoginContext)
+  const [cardItems, setCardItems] = useState([] as CardItemType[])
 
   return (
     <div className="App">
       <Routes>
-      <Route  element={<Layout  />}>
+      <Route  element={<Layout cardItems={cardItems} setCardItems={setCardItems} />}>
        <Route index path='/' element={<Home  />} />
        <Route index path='/login' element={
          userLog.isLogin?
