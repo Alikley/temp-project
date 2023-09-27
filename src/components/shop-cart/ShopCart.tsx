334344ec9cccd59122de3a5ca19d7c014e1c9c78
Alikley <88429641+Alikley@users.noreply.github.com>
2023-09-27 21:45:37 +0330

    Update ShopCart.tsx

--- src/components/shop-cart/ShopCart.tsx
+++ src/components/shop-cart/ShopCart.tsx
@@ -2,15 +2,12 @@ import { useState } from 'react'
 import { useQuery } from 'react-query';
 //components
 
-import  Drawer  from '@mui/material/Drawer';
 import  LinearProgress  from '@mui/material/LinearProgress';
 import  Grid  from '@mui/material/Grid';
-import  Badge  from '@mui/material/Badge';
-import { AddShoppingCart } from '@mui/icons-material';
 //style
-import {Wrapper,StyledButton} from'./App.styled';
+import {Wrapper} from'./App.styled';
 import Item from './item/Item';
-import Cart from './cart/Cart';
+import Layout from '../layout/Layout';
 //type
  export interface CardItemType{
   id:number;
@@ -26,15 +23,13 @@ const getProducts = async (): Promise<CardItemType[]> =>
 await (await fetch('https://fakestoreapi.com/products')).json();
 
 function ShopCart() {
-  const [cartOpen, setCartOpen] = useState(false)
   const [cardItems, setCardItems] = useState([] as CardItemType[])
 
   const {data, isLoading , error} = useQuery<CardItemType[]>('products',getProducts);
  
     console.log(data);
 
-    const getTotalItems = (items: CardItemType[]) =>
-    items.reduce((ack:number,items) => ack + items.amount, 0)
+  
 
     const handleAddToCart = (clickedItem:CardItemType) =>{
       setCardItems(prev => {
@@ -53,18 +48,7 @@ function ShopCart() {
       })
     };
 
-    const handleRemoveFromCart = (id:number) => {
-      setCardItems(prev => (
-        prev.reduce((ack , item) => {
-          if(item.id === id){
-            if(item.amount === 1) return ack;
-            return [...ack, {...item,amount:item.amount - 1}]
-          }else{
-            return [...ack,item]
-          }
-        }, [] as CardItemType[])
-      ))
-    };
+
     
     if(isLoading) return <LinearProgress />;
     if (error) return <div>Something Went Wrong ...</div>
@@ -72,18 +56,7 @@ function ShopCart() {
   return (
     <div className="App">
       <Wrapper>
-        <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
-          <Cart 
-          cartItem={cardItems} 
-          addToCart={handleAddToCart}
-          removeFromCart={handleRemoveFromCart}
-          />
-        </Drawer>
-        <StyledButton onClick={() => setCartOpen(true)} >
-            <Badge badgeContent={getTotalItems(cardItems)} color='error'>
-              <AddShoppingCart />
-            </Badge>
-        </StyledButton>
+        <Layout cardItems={cardItems} setCardItems={setCardItems}  />
         <Grid container spacing={3}>
           {data?.map(item => (
             <Grid item key={item.id} xs={12} sm={4}>
@@ -93,7 +66,7 @@ function ShopCart() {
         </Grid>
       </Wrapper>
     </div>
-  );
+  ); 
 }
 
 export default ShopCart;
