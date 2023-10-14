607a627062a4af2268c417265c25a4d1898ef143
Alikley <alikley933@gamail.com>
2023-10-14 20:44:26 +0330

    new

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -104,7 +104,7 @@ const Home = () => {
               responsive={responsive}
               infinite={true}
               autoPlay={true}
-              autoPlaySpeed={600}
+              autoPlaySpeed={500}
               keyBoardControl={true}
               customTransition="all .5"
               transitionDuration={500}
