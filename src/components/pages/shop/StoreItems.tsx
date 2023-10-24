544d7c6e8241a85458d2587e1c1234f0f9c35fb0
Mohammad Ali Mohammadi <88429641+Alikley@users.noreply.github.com>
2023-10-24 21:59:03 +0330

    Update StoreItems.tsx

--- src/components/pages/shop/StoreItems.tsx
+++ src/components/pages/shop/StoreItems.tsx
@@ -1,45 +1,74 @@
 import  './scss/storeItems.scss'
-import { Button, Card} from 'react-bootstrap';
+import { Button, Card, Col, Container, Nav, Row} from 'react-bootstrap';
 import { formatCurrency } from '../../../utilities/formatCurrency';
-import { useShopingCart } from '../../../context/ShopingCartContext';
+// import { useShopingCart } from '../../../context/ShopingCartContext';
+// import { useQuery } from 'react-query';
+import  LinearProgress  from '@mui/material/LinearProgress';
+import { useState,useEffect } from 'react';
+import ShowProducts from './ShowProducts';
 // https://preview.colorlib.com/#essence
-interface storeitemsProps{
-    id:number;
-    name:string;
-    price:number;
-    imgURL:string;
-}
+export interface CardItemType{
+  id:number;
+  category:string
+  description:string;
+  image:string;
+  price:number;
+  title:string
+  amount:number
+ }
 
-const StoreItems = ({id,name,price,imgURL}: storeitemsProps) => {
-  const {getItemQuantity,increaseCartQuantity,decreaseCartQuantity,removeFromCart} = useShopingCart()
-  const quantity = getItemQuantity(id)
+
+
+const StoreItems = () => {
+  const [data, setData] = useState([] as CardItemType[])
+  const [filter, setfilter] = useState(data)
+  const [loading,setLoading] = useState(false)
+  let componentMounted = true;
+
+
+
+
+
+
+  useEffect(() => {
+    const getProducts = async () => {
+      setLoading(true)
+      const response = await fetch('https://fakestoreapi.com/products');
+      if(componentMounted){
+        setData(await response.clone().json());
+        setfilter(await response.json())
+        setLoading(false);
+        console.log(filter);
+        
+      }
+      return () => {
+        componentMounted = false;
+      }
+    }
+    getProducts();
+  },[])
+
+
+
+
+
+
+    const Loading = () => {
+      return(
+        <>
+          <LinearProgress />
+        </>
+      )
+    }
+    
+
   return (
     <div className='storeItems'>
-        <Card className='cart-item'>
-            <Card.Img variant='top' src={imgURL} className='img-item' />
-             <Card.Body className='d-flex flex-column'>
-                <Card.Title className=' justify-content-space-between align-items-baseline mb-4'>
-                    <span className='fs-2'>{name}</span>
-                    <br />
-                    <span className='ms-2 text-muted price-item'>{formatCurrency(price)}</span>
-                </Card.Title>
-                <div className="mt-auto">
-                  {quantity === 0 ? (
-                      <Button className='button-item' onClick={() => increaseCartQuantity(id)}>Add To Cart</Button>
-                  ) :( <div className='d-flex align-items-center flex-column' style={{gap:".5rem"}}>
-                          <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
-                            <div>
-                              <span className='fs-3'>{quantity}</span> in Cart
-                            </div>
-                          <Button onClick={() => increaseCartQuantity(id)}>+</Button>
-                        <div className='d-flex align-items-center justify-content-center' style={{gap:".5rem"}}>
-                          <Button variant='danger' size='sm' onClick={() => removeFromCart(id)}>Remove</Button>
-                        </div>
-                    </div>
-                    )}
-                </div>
-             </Card.Body>
-        </Card>
+      <Container>
+        <Row className='justify-content-center' >
+          {loading ? <Loading /> : <ShowProducts filter={filter} />}
+        </Row>
+      </Container>
     </div>
   )
 }
